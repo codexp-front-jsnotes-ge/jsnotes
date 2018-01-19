70a11407243620e3160d6220df67a51d0eb5ef12
rafaeltanaka <rafaeltanaka@hotmail.com>
2018-01-19 21:40:06 -0200

    Continuação JS

--- js/jsnotes.js
+++ js/jsnotes.js
@@ -1,6 +1,24 @@
-let contaItem = 0
-function incluirItem() {
-    contaItem++    
-    let item = '<label for="item__title" class="item__title">ITEM ' + contaItem + '</label><input type="text" id="item__text" class="item__text"><i class="far fa-minus-square item__minus"></i>';
-    document.getElementById('item').innerHTML += item;
+var item = []
+var contaItem = 0
+
+function incluirCampo() {
+    item.push('<label for="item__title" class="item__title">ITEM ' + (contaItem + 1) + '</label><input type="text" id="item__text" class="item__text"><i class="far fa-minus-square item__minus" onclick="excluirCampo(' + contaItem + ')"></i>');
+    listarCampos();
 }
+
+
+function listarCampos() {
+    // document.getElementById('item').innerHTML = item;
+    // contaItem++;
+    // console.log(item);
+    document.getElementById('item').innerHTML = null;
+    for (contaItem = 0; contaItem < item.length; contaItem++) {
+        document.getElementById('item').innerHTML += item[contaItem];
+    }
+    console.log(item);
+};
+function excluirCampo(contaItem) {
+    document.getElementById('item').innerHTML = null;
+    item.splice(contaItem, 1);
+    listarCampos();
+};
